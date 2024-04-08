33d714d4e91809d03df46f3c6ed5788294acfa7c
lachdab <76220872+lachdab@users.noreply.github.com>
2024-04-08 05:14:28 +0200

    Initial commit

---
+++ Cheat/dllmain.cpp
@@ -0,0 +1,22 @@
+// dllmain.cpp : Definiert den Einstiegspunkt für die DLL-Anwendung.
+#include <Windows.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        // here comes the call for the cheat code and the menu with imgui
+        MessageBox(NULL, "Injected into Game", "Success!", NULL);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ Cheat/main.cpp
@@ -0,0 +1,10 @@
+//1F34654772C => Ammo (Client side)
+//1F4C6543290 => Shield (Server side)
+//1F4C6543288 => Health (Server side)
+#include <Windows.h>
+
+
+// entry point where the cheat code get's injected into the game
+void Run() {
+
+}
